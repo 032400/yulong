3870c53f7e1bc9e69db2ce16c39b94b148b105d6
ring <2522564501@qq.com>
2021-11-25 08:52:08 +0800

    删除查看更多

--- src/views/details/details.vue
+++ src/views/details/details.vue
@@ -269,21 +269,10 @@
             </a-form-model>
           </div>
         </div>
-        <div class="go">
-          <p>上一篇：航空方面有哪些专业？</p>
-          <p>下一篇：航空专业就业前景如何？</p>
-        </div>
       </div>
     </div>
      <div class="cdsa">
       <aviation />
-      <div class="information_con_btm">
-          <div class="btm_border">
-              <span>查看更多问题</span>
-              <img src="@/assets/img/公共/sou.png" alt="">
-          </div>
-      </div>
-     <!-- <div class="click">更多院校></div>-->
     </div> 
     <div class="read">
       <div class="read-top">
@@ -799,13 +788,6 @@ export default {
       padding-left: 0.25rem;
       padding-bottom: 0.05rem;
     }
-    .go {
-      margin-left: 0.16rem;
-      p {
-        margin-bottom: 0.15rem;
-        color: rgb(51, 51, 51);
-      }
-    }
   }
 }
 .cdsa {
@@ -816,49 +798,6 @@ export default {
   box-shadow: 0 0 8px #ccc;
   background: #fff;
 }
-
-   .information_con_btm{
-      padding: .15rem .12rem .25rem .12rem;
-      .btm_border{
-          width: 100%;
-          height: 100%;
-          border-radius: 5px;
-          border: solid 1px #1776af;
-          text-align: center;
-          line-height: .42rem;
-          span{
-              font-size: .16rem;
-              color: #1776af;
-              font-family: PingFang-SC-Medium;
-              font-weight: normal; 
-              font-stretch: normal;
-              letter-spacing: 0px;
-          }
-          img{
-              width: 0.16rem;
-              height: 0.16rem;
-              margin-left: .04rem; 
-              vertical-align: middle;
-              margin-bottom: .02rem;   
-          }
-      }
-      :hover{
-          background: #3f555f;
-      }
-      :hover>span{
-          color: #ffffff;
-          font-size: .16rem;
-          font-family: PingFang-SC-Medium;
-          font-weight: normal; 
-          font-stretch: normal;
-          letter-spacing: 0px;
-      }
-  }
-// .click {
-//   text-align: center;
-//   color: rgb(153, 153, 153);
-//   margin: 0.18rem 0;
-// }
 .read {
   width: 3.55rem;
   margin: 0 auto 0.15rem;
