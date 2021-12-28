bbfc005cfa6814c9d65f6af2ceaf02534be72eb2
li <1223757604@qq.com>
2021-12-28 14:08:36 +0800

    修改bug

--- src/views/sichuan/fours.vue
+++ src/views/sichuan/fours.vue
@@ -3,7 +3,14 @@
     <div class="int_con">
       <img :src="url+img[1]" />
     </div>
-    <Pages />
+    <div class="title_warp">
+      <p>
+        <router-link to="/" tag="span">当前位置：首页</router-link>
+        <router-link  to="" tag="span">
+            <span style="color: #999;">>{{name}}</span>
+        </router-link>
+      </p >
+    </div>
 
     <All />
     
@@ -12,19 +19,20 @@
   </div>
 </template>
 <script>
-import Pages from "@/components/page/index.vue";
+// import Pages from "@/components/page/index.vue";
 import All from "@/views/sichuan/fours/fourall.vue";
 import axios from 'axios'
 export default {
   components: {
-    Pages,
+    // Pages,
     All,
   },
   data(){
     return {
       
       url:"http://39.105.137.169:9527/",
-      img:''
+      img:'',
+      name:''
     }
   },
   methods:{
@@ -33,6 +41,7 @@ export default {
   mounted(){
    axios.get("/cw", { params: { mod: "center",gid:this.$route.query.gid } }).then((res) => {
       this.img = res.data[0].product_album.split(',')
+      this.name = res.data[0].product_name
     });
   }
 };
@@ -47,5 +56,32 @@ export default {
     width: 3.75rem;
     height: 1.5rem;
   }
+}
+.title_warp{
+width:100%;
+padding:0 .2rem;
+margin: .15rem 0;
+box-sizing:border-box;
+  p{
+    width: 3.75rem;
+    margin: 0 auto;
+    span {
+        margin-left: 0.2rem;
+        font-size: .14rem;
+        font-weight: normal;
+        font-stretch: normal;
+        line-height: .07rem;
+        letter-spacing: 0px;
+        color: #888888;
+    }
+    .hang {
+        margin-left: 0.05rem;
+        font-family: PingFang-SC-Medium;
+        font-size: .14rem;
+        font-weight: normal;
+        letter-spacing: 0px;
+        color: #b0b0b0;
+        }
+    }
 }
 </style>
